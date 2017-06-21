34918cb7b145929a0fdcf5070d21cece3dd3f8c0
VladimirLazic <ironmanm72@gmail.com>
2017-06-21 07:50:02 +0200

    Documentation done

---
+++ CMakeFiles/reciever.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reciever.dir/src/segmenter.o"
+  "CMakeFiles/reciever.dir/src/network.o"
+  "CMakeFiles/reciever.dir/src/reciever.o"
+  "reciever.pdb"
+  "reciever"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/reciever.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
